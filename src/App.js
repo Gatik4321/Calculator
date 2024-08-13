baab6216b791e6eca210434b18bc73f490a345ed
Gatik Alagh <156594667+Gatik4321@users.noreply.github.com>
2024-08-13 23:26:48 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Calculator from './Calculator';
+import React from 'react';
+function App() {
+  return (
+    // Now we are going to design calculator app using react js
+    <div>
+        <Calculator/>
+    </div>
+  );
+}
+
+export default App;
